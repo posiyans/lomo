e39070e042235e221c5e9c83b90eefc4aa8caa96
posiyans <posiyans@gmail.com>
2019-06-28 17:37:40 +0300

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\MeteringDevice;
+use App\Models\Gardening;
+use App\Models\Stead;
+use App\Models\ReceiptType;
+use App\Models\Rate;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
