19465a87d0f18af9d7a27f1a96cb34e0b196ebfd
posiyans <posiyans@gmail.com>
2020-12-19 19:04:58 +0300

    19-12-2020

---
+++ routes/myRoutes/all/voting.php
@@ -0,0 +1,7 @@
+<?php
+
+//Route::resource('user/category', 'User\CategoryController')->only(['index']);
+//Route::resource('user/article', 'User\ArticleController')->only(['index', 'show']);
+//Route::resource('user/comment', 'User\CommentController')->only(['index']);
+Route::resource('user/voting', 'User\Voting\UserVotingController')->only(['index', 'show']);
+Route::post('vote', 'User\Voting\UserVotingController@addAnswer');
