b6d29f97069ef62c70e395ec68932466ec7c1012
posiyans <posiyans@gmail.com>
2024-03-12 17:12:53 +0300

    _

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,6 +40,8 @@ class DatabaseSeeder extends Seeder
         ]);
         if ($demo) {
             $this->call([
+                CategorySeeder::class,
+                SiteMenuSeeder::class,
                 GardeningDemoSeeder::class,
                 UserModelDemoSeeder::class,
                 SteadModelDemoSeeder::class,
